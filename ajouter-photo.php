e2adbd08f39f736afa25c0ba3370d2891ef3de03
lchaste <82218335+lchaste@users.noreply.github.com>
2021-04-25 16:49:31 +0200

    Update ajouter-photo.php

--- ajouter-photo.php
+++ ajouter-photo.php
@@ -1,18 +1,25 @@
-<?php require_once("connexion_base.php"); ?>
-
 <?php
-if (!empty($_POST['legende']))
+require_once("connexion_base.php");
+$donnees['titre_page'] = "Récapitulatif photo";
+$pseudo = $_POST['pseudo'];
+
+include "debut-page.inc.php";
+ ?>
+<?php
+if (!empty($_POST['pseudo']))
 {
-    $nom = $_POST['legende'];
+        $nom = $_POST['pseudo'];
     // exécuter une requete MySQL de type INSERT
-    $requete="INSERT INTO projet_photo (id_membre, id_fiche, legende, valide) VALUES (?,NOW())";
+    $requete="INSERT INTO projet_photo (id_membre, id_fiche, legende)  WHERE projet_photo.id_membre= projet_membre.id VALUES (?,NOW())";
     $reponse=$pdo->prepare($requete);
     $reponse->execute(array($nom));
-    echo "<p>L'oeuvre de votre enfant a été ajoutée.</p>";
-    echo "<p>Merci de votre contribution à bientôt sur Fun Kids</p>";
+    echo "<p>Le chef d'oeuvre de votre enfant a été ajouté.</p>";
+    echo "<p>Merci pour votre participation, à bientôt sur Fun Kids!</p>";?>
+    Vous allez revenir sur la page pricipale des <a href="activité.php">Activités</a>
 
+    <?php
     $dernier_id = $pdo->lastInsertId();
-    
+
     if(!empty($_FILES['fichier']['tmp_name']))
     {
         $size = getimagesize($_FILES['fichier']['tmp_name']);
@@ -20,16 +27,16 @@ if (!empty($_POST['legende']))
         echo "Filetype : ".$size['mime'];
         if ($size['mime'] == "image/jpeg")
         {
-            $uploaddir = $_SERVER['DOCUMENT_ROOT']."/cswd/projet/projet/images-ajoutées";
+            $uploaddir = $_SERVER['DOCUMENT_ROOT']."/cswd/projet/projet/images-ajoutées/";
             $uploadfile = "activité-".$dernier_id.".jpg";
             if (move_uploaded_file($_FILES['fichier']['tmp_name'], $uploaddir.$uploadfile))
             {
-            echo "<p>Votre image de l'activité a bien été ajoutée : ".$uploadfile."</p>";
+            echo "<p>L'activité a bien été ajouté : ".$uploadfile."</p>";
             }
-        else
-        {
+            else
+            {
             echo "<p>Probleme sur le serveur : ".$uploaddir."</p>";
-        }
+            }
         }
         else
         {
@@ -38,8 +45,7 @@ if (!empty($_POST['legende']))
     }
     else
     {
-    echo "<p>Pas de fichier spécifié.</p>";
+        echo "<p>Pas de fichier spécifié.</p>";
     }
-
 }
 ?>
