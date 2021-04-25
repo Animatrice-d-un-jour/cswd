1bfbc2bf20c5e0b57b9c8b25a22d335e97fafe1a
lchaste <82218335+lchaste@users.noreply.github.com>
2021-04-25 16:48:47 +0200

    Update detail-activité.php

--- detail-activité.php
+++ detail-activité.php
@@ -26,7 +26,7 @@ include "debut-page.inc.php";
     </ol>
   </nav>
 
-    <h1> <?php echo $enregistrements[0]['titre']?> </h1>
+    <h1 align = "center"> <?php echo $enregistrements[0]['titre']?> </h1>
 
     <div class="container">
       <div class="row">
@@ -57,19 +57,30 @@ include "debut-page.inc.php";
       </div>
       <div class="row">
         <div class="col">
-          <h6> Les résultats de vos bouts de chou:</h6>
-            <form action="ajouter-photo.php" enctype="multipart/form-data" method="post">
-            <input type="text" name="nom" /><br />
-                <input type="hidden" name="MAX_FILE_SIZE" value="3000000" />
+
+          <div class="formulaire_question">
+          <h6 id="gras">Les résultats de vos bouts de chou:</h6>
+          <form action="ajouter-photo.php" enctype="multipart/form-data" method="post">
+              <div class="form-floating mb-3">
+                <input type="text" class="form-control" placeholder="Votre pseudo" required="required" id="pseudo" name="pseudo">
+                <label for="pseudo">Votre pseudo</label>
+              </div>
+              <div class="form-floating mb-3">
+                <input type="hidden" name="MAX_FILE_SIZE" value="3000000" required="required" />
                 <input type="file" name="fichier" /><br />
-                <input type="submit" />
-            </form>
+              </div>
+              <div class="col-auto">
+              <button type="submit" class="btn btn-primary rounded-pill">Envoyer</button>
+              </div>
+            </div>
+          </form>
+          </div>
         </div>
       </div>
       <div class="row">
         <div class="col">
           <h6> Les commentaires des internautes:</h6>
-          // ajouter les commentaires des internautes
+          //inclure les commentaires!
         </div>
       </div>
       <div class="row">
@@ -98,4 +109,5 @@ include "debut-page.inc.php";
 </main>
 
 <?php include "fin-page.inc.php"; ?>
+
 
