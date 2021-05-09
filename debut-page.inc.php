4492333c5f8721fbe284256dfbbac6f09fdfb6f7
chlmorel <82218408+chlmorel@users.noreply.github.com>
2021-05-10 00:21:15 +0200

    Update debut-page.inc.php

--- debut-page.inc.php
+++ debut-page.inc.php
@@ -12,7 +12,13 @@
     <title><?php echo $donnees['titre_page']; ?></title>
   </head>
   <body>
-    <header>
+    <a href="#hautdepage"><button type="button" class="stick">
+      <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-arrow-up-circle-fill" viewBox="0 0 16 16" id="flèche">
+      <path d="M16 8A8 8 0 1 0 0 8a8 8 0 0 0 16 0zm-7.5 3.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707V11.5z"/>
+      </svg>
+    </button></a>
+
+    <header id="hautdepage">
       <!--- Première barre de navigation avec le logo et les icones favoris, compte -->
       <nav class="navbar navbar-light bg-white">
         <div class="container">
@@ -33,8 +39,10 @@
             </form>
             </div>
 
+            <div class="picto">
+
             <div class="case">
-              <a href="connexion.php">  <!--- il faudra ajouter un lien -->
+              <a href="#">  <!--- il faudra ajouter un lien -->
                   <!--- Icone membre -->
                 <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16" id="people">
                 <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
@@ -49,6 +57,7 @@
                 <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/>
                 </svg>
               </a>
+            </div>
             </div>
           </div>
         </div>
