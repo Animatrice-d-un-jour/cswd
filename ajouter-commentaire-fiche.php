6b7d2a76b3426c5f18dbdfb1826ce3c2255f36f8
chlmorel <82218408+chlmorel@users.noreply.github.com>
2021-05-11 01:59:02 +0200

    Update ajouter-commentaire-fiche.php

--- ajouter-commentaire-fiche.php
+++ ajouter-commentaire-fiche.php
@@ -1,8 +1,11 @@
 <?php
+session_start();
 require_once("connexion_base.php");
 $donnees['titre_page'] = "Récapitulatif commentaire";
-$pseudo = $_POST['pseudo'];
+$titre = $_POST['titre'];
 $commentaire = $_POST['commentaire'];
+$id_membre = $_SESSION['id_projet_membre'];
+$fiche = $POST['fiche'];
 
 include "debut-page.inc.php";
  ?>
@@ -17,17 +20,17 @@ include "debut-page.inc.php";
 <h2>Récapitulatif</h2>
 <p>Votre commentaire a été envoyé !  Nous le publierons dans les plus brefs délais. </p>
 <ul>
-  <li>Votre pseudo : <?php echo $pseudo; ?></li>
+  <li>Titre : <?php echo $titre; ?></li>
   <li>Votre commentaire : <?php echo $commentaire; ?></li>
 </ul>
 
 <?php
 
 // exécuter une requete MySQL de type INSERT
-$requete="INSERT INTO projet_commentaire (id_menbre,id_fiche,texte,titre,datecreation) WHERE projet_commentaire.id_membre=projet_membre.id AND projet_commentaire.id_fiche=projet_fiche.id
-VALUES (?, ?, ?, ?,NOW())";
+$requete="INSERT INTO projet_commentaire (id_membre,id_fiche,titre, texte, date,valide) 
+VALUES (?, ?, ?, ?, ?, NOW())";
 $reponse=$pdo->prepare($requete);
-$reponse->execute(array($pseudo, $commentaire, 0));
+$reponse->execute(array($id_membre, $fiche, $titre, $commentaire, 0));
 ?>
 
 <p>Retourner à la page des <a href="fiche.php">Activités et jeux</a></p>
