a78a0be27c8801449b5df031f5b4e27fb6ddbb41
chlmorel <82218408+chlmorel@users.noreply.github.com>
2021-05-10 15:02:52 +0200

    Update detail-fiche.php

--- detail-fiche.php
+++ detail-fiche.php
@@ -3,9 +3,9 @@ require_once("connexion_base.php");
 
 $id = $_GET['id']; // Récupération de paramètres de type $_GET
 
-$requete = "SELECT projet_fiche.*,tranche_age,nom FROM projet_fiche,projet_age,projet_theme WHERE projet_fiche.id_age = projet_age.id AND projet_fiche.id_theme = projet_theme.id AND projet_fiche.id = $id;";
+$requete = "SELECT projet_fiche.*,tranche_age,nom FROM projet_fiche,projet_age,projet_theme WHERE projet_fiche.id_age = projet_age.id AND projet_fiche.id_theme = projet_theme.id AND projet_fiche.id = ?;";
 $reponse = $pdo->prepare($requete);
-$reponse->execute();
+$reponse->execute(array($id));
 // récupérer tous les enregistrements dans un tableau
 $enregistrements = $reponse->fetchAll();
 // connaitre le nombre d'enregistrements
@@ -17,12 +17,22 @@ include "debut-page.inc.php";
 ?>
 
 
-<main id="fiches">
+<main class="fiches">
 
   <nav aria-label="breadcrumb">
     <ol class="breadcrumb">
       <li class="breadcrumb-item"><a href="index.php">Accueil</a></li>
-      <li class="breadcrumb-item"><a href="fiche.php">Activités et Jeux</a></li>
+
+      <?php if($enregistrements[0]['id_type']==1)
+        {
+       ?> <li class="breadcrumb-item"><a href="fiche.php?id=<?php echo $enregistrements[0]['id_type']; ?>">Jeux</a></li>
+     <?php
+        }
+        else if($enregistrements[0]['id_type']==2)
+        { ?>
+          <li class="breadcrumb-item"><a href="fiche.php?id=<?php echo $enregistrements[0]['id_type']; ?>">Activités</a></li>
+        <?php
+        } ?>
       <li class="breadcrumb-item active" aria-current="page"><?php echo $enregistrements[0]['titre']?></li>
     </ol>
   </nav>
@@ -42,16 +52,34 @@ include "debut-page.inc.php";
             </div>
           </div>
           <div class="row">
-            <div class="col">
-            /!\ ici il faut ajouter if file_exist... que je n'ai pas réussi /!\
-            </div>
-            <div class="col">
-              <h5 class="materiel"> Vous avez besoin de: </h5>
-                <?php echo $enregistrements[0]['materiel']?>
-            </div>
+              <?php
+
+              if (file_exists("images/activites/".$enregistrements[0]['id'].".jpg"))
+                {?>
+                  <div class="col" id ="image-activite">
+                  <img src="images/activites/<?php echo $enregistrements[0]['id'] ?>.jpg" alt="activité-<?php echo $enregistrements[0]['id'] ?>" width="250px">
+                  </div>
+
+                  <div class="col">
+                    <h5 class="materiel"> Vous avez besoin de : </h5>
+                      <?php echo $enregistrements[0]['materiel']?>
+                  </div>
+                <?php
+              }
+              else
+              {?>
+                <div class="row">
+                  <div class="col">
+                    <h5 class="materiel2"> Vous avez besoin de : </h5>
+                      <?php echo $enregistrements[0]['materiel']?>
+                  </div>
+                </div>
+                <?php
+              } ?>
+              
             <div class="row">
               <div class="col">
-                <h6 class="deroulement"> Déroulement de l'activité: </h6>
+                <h5 class="deroulement"> Déroulement de l'activité : </h5>
                 <?php echo $enregistrements[0]['deroulement']?>
               </div>
             </div>
@@ -81,9 +109,9 @@ include "debut-page.inc.php";
                 <h6> Les commentaires des internautes:</h6>
                 <?php
                 // exécuter une requete MySQL
-                $requete = "SELECT projet_commentaire.titre, projet_commentaire.texte, projet_commentaire.date FROM projet_commentaire WHERE projet_commentaire.id_membre=projet_membre.id AND projet_commentaire.id_fiche=projet_fiche.id AND valide=1;";
+                $requete = "SELECT projet_commentaire.titre, projet_commentaire.texte, projet_commentaire.date FROM projet_commentaire WHERE projet_commentaire.id_membre=projet_membre.id AND projet_commentaire.id_fiche=projet_fiche.id AND valide=?;";
                 $reponse = $pdo->prepare($requete);
-                $reponse->execute();
+                $reponse->execute(array(1));
                 // récupérer tous les enregistrements dans un tableau
                 $enregistrements = $reponse->fetchAll();
                 // connaitre le nombre d'enregistrements
