e6789a4870b2b6c3a51016e3c76e5f15b05ae750
chlmorel <82218408+chlmorel@users.noreply.github.com>
2021-05-10 20:27:50 +0200

    Update favoris.php

--- favoris.php
+++ favoris.php
@@ -5,62 +5,91 @@ require_once("connexion_base.php");
 
 include "debut-page.inc.php";
 ?>
+<main class="fiches">
+
+<h2>Vos favoris</h2>
+
 <?php
   if (isset($_SESSION['id_projet_membre']))
   {
-    $reussi = false;
-    if (projet_membre_fiche.id_membre= $_SESSION['id_projet_membre'])
-
-      $requete = "SELECT projet_fiche.*,tranche_age,nom FROM projet_fiche,projet_age,projet_theme,projet_membre_fiche WHERE projet_fiche.id_age = projet_age.id AND projet_fiche.id_theme = projet_theme.id AND projet_fiche.id = projet_membre_fiche.id_fiche AND projet_membre_fiche.id_membre= $_SESSION['id_projet_membre'];";
+      $id_membre = $_SESSION['id_projet_membre'];
+      $requete = "SELECT * FROM projet_membre_fiche WHERE  projet_membre_fiche.id_membre= ?;";
       $reponse = $pdo->prepare($requete);
-      $reponse->execute(array($id,1));
+      $reponse->execute(array($id_membre));
       // récupérer tous les enregistrements dans un tableau
       $enregistrements = $reponse->fetchAll();
       // connaitre le nombre d'enregistrements
       $nombreReponses = count($enregistrements);
-       ?>
-      <main class="fiches">
 
-        <nav aria-label="breadcrumb">
-          <ol class="breadcrumb">
-            <li class="breadcrumb-item"><a href="index.php">Accueil</a></li>
-            <li class="breadcrumb-item active" aria-current="page">Favoris</li>
-          </ol>
-        </nav>
+      if ($nombreReponses==0)
+      {
+        echo "<p>Vous n'avez pas encore de favoris. N'hésitez pas à faire un tour pour sélectionner des activités et jeux ! </p>";
+      }
+      else
+      {
+        $requete = "SELECT projet_fiche.*,tranche_age,nom FROM projet_fiche,projet_age,projet_theme, projet_membre_fiche WHERE projet_fiche.id_age = projet_age.id AND projet_fiche.id_theme = projet_theme.id AND validation =? AND projet_membre_fiche.id_membre = ? AND projet_membre_fiche.id_fiche = projet_fiche.id;";
+        $reponse = $pdo->prepare($requete);
+        $reponse->execute(array(1,$id_membre));
+        // récupérer tous les enregistrements dans un tableau
+        $favoris = $reponse->fetchAll();
+        // connaitre le nombre d'enregistrements
+        $nombreReponses = count($favoris);
+        // parcourir le tableau des enregistrements ?>
+
         <div class="cards">
-        <!-- boucle -->
+          <!-- boucle -->
 
-        <?php
+          <?php
+          for ($i=0; $i<count($favoris); $i++)
+          {
+            if($favoris[$i]['pseudo']=='')
+            {
+          ?>
+              <div class="card">
+                <div class="card-body">
+                  <h5 class="card-title"><?php echo $favoris[$i]['titre'];?></h5>
+                  <p class="card-text">
+                    AGE : <?php echo $favoris[$i]['tranche_age'] ?> <br>
+                    DUREE : <?php echo $favoris[$i]['duree'] ?><br>
+                    THEME : <?php echo $favoris[$i]['nom'] ?><br>
+                  </p>
+                </div>
+                <div>
+                  <a href="detail-fiche.php?id=<?php echo $favoris[$i]['id'] ?>" class="btn btn-primary rounded-pill espace">Consulter</a>
+                </div>
+                </div>
+            <?php
+             }
+             else
+             { ?>
+                 <div class="card">
+                   <div class="card-body">
+                     <h5 class="card-title"><?php echo $favoris[$i]['titre']." (@". $favoris[$i]['pseudo'].")";?></h5>
+                     <p class="card-text">
+                       AGE : <?php echo $favoris[$i]['tranche_age'] ?> <br>
+                       DUREE : <?php echo $favoris[$i]['duree'] ?><br>
+                       THEME : <?php echo $favoris[$i]['nom'] ?><br>
+                     </p>
+                   </div>
+                   <div>
+                     <a href="detail-fiche.php?id=<?php echo $favoris[$i]['id'] ?>" class="btn btn-primary rounded-pill color">Consulter</a>
+                   </div>
+                   </div>
+                   <?php
+                  }
 
-        for ($i=0; $i<count($enregistrements); $i++)
-        {
-        ?>
-        <div class="card">
-          <div class="card-body">
-            <h5 class="card-title"><?php echo $enregistrements[$i]['titre'];?></h5>
-            <p class="card-text">
-              AGE : <?php echo $enregistrements[$i]['tranche_age'] ?> <br>
-              DUREE : <?php echo $enregistrements[$i]['duree'] ?><br>
-              THEME : <?php echo $enregistrements[$i]['nom'] ?><br>
-            </p>
-          </div>
-          <div>
-            <a href="detail-fiche.php?id=<?php echo $enregistrements[$i]['id'] ?>" class="btn btn-primary rounded-pill espace">Consulter</a>
-            <a href="supprimer_favoris.php" class="btn btn-primary rounded-pill espace">Supprimer</a>
-          </div>
+              } ?>
+
           </div>
           <?php
-         } ?>
-        </div>
-      </main>
-    <?php
-    }
-    else:
-    {
-      echo "Vous n'avez aucun favoris";
-    }?>
-  <?php else:
-    header("location:connexion.php");
-  ?>
+        }
+      }
+      else
+        {
+          echo "<p>Veuillez tout d'abord vous créez un compte pour ajouter des activités et jeux en favoris.</p>";
+          echo"<a href=\"inscription-formulaire.php\">S'inscrire</a>";
+        }
+      ?>
+  </main>
 
 <?php include "fin-page.inc.php"; ?>
