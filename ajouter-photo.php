8d758d1ad0e9a973f946265d3a0e8b1e6d86cf7f
lchaste <82218335+lchaste@users.noreply.github.com>
2021-05-10 15:45:37 +0200

    Update ajouter-photo.php

--- ajouter-photo.php
+++ ajouter-photo.php
@@ -8,14 +8,14 @@ include "debut-page.inc.php";
 <?php
 if (!empty($_POST['pseudo']))
 {
-        $nom = $_POST['pseudo'];
+     $nom = $_POST['pseudo'];
     // exécuter une requete MySQL de type INSERT
     $requete="INSERT INTO projet_photo (id_membre, id_fiche, legende)  WHERE projet_photo.id_membre= projet_membre.id VALUES (?,NOW())";
     $reponse=$pdo->prepare($requete);
     $reponse->execute(array($nom));
     echo "<p>Le chef d'oeuvre de votre enfant a été ajouté.</p>";
     echo "<p>Merci pour votre participation, à bientôt sur Fun Kids!</p>";?>
-    Vous allez revenir sur la page pricipale des <a href="activité.php">Activités</a>
+    Vous allez revenir sur la page pricipale des <a href="fiche.php">Activités et jeux</a>
 
     <?php
     $dernier_id = $pdo->lastInsertId();
@@ -27,7 +27,7 @@ if (!empty($_POST['pseudo']))
         echo "Filetype : ".$size['mime'];
         if ($size['mime'] == "image/jpeg")
         {
-            $uploaddir = $_SERVER['DOCUMENT_ROOT']."/cswd/projet/projet/images-ajoutées/";
+            $uploaddir = $_SERVER['DOCUMENT_ROOT']."/cswd/projet/images-ajoutées/";
             $uploadfile = "activité-".$dernier_id.".jpg";
             if (move_uploaded_file($_FILES['fichier']['tmp_name'], $uploaddir.$uploadfile))
             {
