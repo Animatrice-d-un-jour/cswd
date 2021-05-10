60e8233ed6ff831fe2d8b18c9b94231ffb1f46e5
chlmorel <82218408+chlmorel@users.noreply.github.com>
2021-05-10 20:27:00 +0200

    Update detail-fiche2.php

--- detail-fiche2.php
+++ detail-fiche2.php
@@ -1,6 +1,7 @@
 <?php
-session_start();
 require_once("connexion_base.php");
+session_start();
+$id = $_GET['id']; // Récupération de paramètres de type $_GET
 
 $requete = "SELECT projet_fiche.*,tranche_age,nom FROM projet_fiche,projet_age,projet_theme WHERE projet_fiche.id_age = projet_age.id AND projet_fiche.id_theme = projet_theme.id AND projet_fiche.id = ?;";
 $reponse = $pdo->prepare($requete);
@@ -15,197 +16,152 @@ $donnees['titre_page'] =  $enregistrements[0]['titre'];
 include "debut-page.inc.php";
 ?>
 
-
 <main class="fiches">
 
   <nav aria-label="breadcrumb">
     <ol class="breadcrumb">
       <li class="breadcrumb-item"><a href="index.php">Accueil</a></li>
-
-      <?php if($enregistrements[0]['id_type']==1)
-        {
-       ?> <li class="breadcrumb-item"><a href="fiche.php?id=<?php echo $enregistrements[0]['id_type']; ?>">Jeux</a></li>
-     <?php
-        }
-        else if($enregistrements[0]['id_type']==2)
-        { ?>
-          <li class="breadcrumb-item"><a href="fiche.php?id=<?php echo $enregistrements[0]['id_type']; ?>">Activités</a></li>
-        <?php
-        } ?>
+      <li class="breadcrumb-item"><a href="activité.php">Activités</a></li>
       <li class="breadcrumb-item active" aria-current="page"><?php echo $enregistrements[0]['titre']?></li>
     </ol>
   </nav>
 
-    <h1 class= "title"> <?php echo $enregistrements[0]['titre']?> </h1>
-    <?php
-    if (isset($_SESSION['id_projet_membre']))
-    {
-    ?>
-      <form class="d-flex" action="" method="post">
-        <input type="hidden" name="fiche" value="<?php echo $id ?>">
-        <input type="hidden" name="membre" value="<?php echo $_SESSION['id_projet_membre']?>">
-        <button type="submit" class = "coeur"><svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16" id="heart">
-        <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/>
-        </button>
-      </form>
-
-      <?php
-      //si la personne clique sur le bouton favoris
-      if (isset( $_POST['fiche']) AND isset( $_POST['membre'])){
-        $fiche = $_POST['fiche'];
-        $membre = $_POST['membre'];
-        //regarder si l'enregistrement existe déjà
-        $requete = "SELECT * FROM projet_membre_fiche WHERE id_membre=? AND id_fiche = ?;";
-        $reponse = $pdo->prepare($requete);
-        $reponse->execute(array($membre, $fiche));
-        // récupérer tous les enregistrements dans un tableau
-        $favoris = $reponse->fetchAll();
-        // connaitre le nombre d'enregistrements
-        $nombreReponses = count($favoris);
-        // parcourir le tableau des enregistrements
-
-        //si l'enregistrement n'existe pas, c'est à dire si la personne n'a pas encore mis l'activité en favoris
-        if ($nombreReponses==0)
-        {
-          $requete="INSERT INTO projet_membre_fiche (id_membre,id_fiche) VALUES (?, ?)";
-          $reponse=$pdo->prepare($requete);
-          $reponse->execute(array($membre, $fiche));
-        }
-      }
+  <div class="fav-tit">
+    <div >
+      <h1 class= "title"> <?php echo $enregistrements[0]['titre']?> </h1>
+    </div>
+    <div>
+    <!--début mécanisme formulaire-->
+    <?php if(isset($_SESSION['id_projet_membre']))
+    { $id_membre = $_SESSION['id_projet_membre'];
       ?>
-      <script type="text/javascript">
-        document.getElementsById("heart1").addEventListener("click", change)
-        function change()
-      {
-        document.getElementsById("heart1").innerHTML =
-      }
-      </script>
-    <?php
-    else {
-      header("location:connexion.php");
-    }
-    ?>
-        <div class="container">
-          <div class="row">
-            <div class="col">
-                <h5 class="theme"> THEME : <?php echo $enregistrements[0]['nom'] ?></h5>
-            </div>
-            <div class="col">
-                <h5 class="age"> AGE : <?php echo $enregistrements[0]['tranche_age'] ?></h5>
-            </div>
-            <div class="col">
-                <h5 class="duree"> DUREE : <?php echo $enregistrements[0]['duree'] ?></h5>
-            </div>
-          </div>
-          <div class="row">
-              <?php
+        <form class="d-flex" action="" method="post">
+          <input type="hidden" name="fiche" value="<?php echo $id ?>">
+          <input type="hidden" name="membre" value="<?php echo $id_membre ?>">
+          <button type="submit" class = "coeur"><svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16" id="heart1">
+          <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z" />
+          </svg>
+        </button>
+        </form>
 
-              if (file_exists("images/activites/".$enregistrements[0]['id'].".jpg"))
-                {?>
-                  <div class="col" id ="image-activite">
-                  <img src="images/activites/<?php echo $enregistrements[0]['id'] ?>.jpg" alt="activité-<?php echo $enregistrements[0]['id'] ?>" width="250px">
-                  </div>
+        <?php
+        //si la personne clique sur le bouton favoris
+        if (isset( $_POST['fiche']) AND isset( $_POST['membre']))
+        {
+          $fiche = $_POST['fiche'];
+          $membre = $_POST['membre'];
 
-                  <div class="col">
-                    <h5 class="materiel"> Vous avez besoin de : </h5>
-                      <?php echo $enregistrements[0]['materiel']?>
-                  </div>
-                <?php
-              }
-              else
-              {?>
-                <div class="row">
-                  <div class="col">
-                    <h5 class="materiel2"> Vous avez besoin de : </h5>
-                      <?php echo $enregistrements[0]['materiel']?>
-                  </div>
-                </div>
-                <?php
-              } ?>
+          //regarder si l'enregistrement existe déjà
+          $requete = "SELECT * FROM projet_membre_fiche WHERE id_membre=? AND id_fiche = ?;";
+          $reponse = $pdo->prepare($requete);
+          $reponse->execute(array($membre, $fiche));
+          // récupérer tous les enregistrements dans un tableau
+          $favoris = $reponse->fetchAll();
+          // connaitre le nombre d'enregistrements
+          $nombreReponses = count($favoris);
+          // parcourir le tableau des enregistrements
 
-            <div class="row">
-              <div class="col">
-                <h5 class="deroulement"> Déroulement de l'activité : </h5>
-                <?php echo $enregistrements[0]['deroulement']?>
+          //si l'enregistrement n'existe pas, c'est à dire si la personne n'a pas encore mis l'activité en favoris
+          if ($nombreReponses==0)
+          {
+            $requete="INSERT INTO projet_membre_fiche (id_membre,id_fiche)
+            VALUES (?, ?)";
+            $reponse=$pdo->prepare($requete);
+            $reponse->execute(array($membre, $fiche));
+          }
+
+       }
+     }
+     else {
+     ?>
+     <a href="inscription-formulaire.php" id="fafa">
+       <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16" id="heart2">
+       <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/>
+       </svg>
+     </a>
+   <?php } ?>
+   <!--fin mécanisme formulaire-->
+   </div>
+ </div>
+
+    <div class="container">
+      <div class="row">
+        <div class="col">
+            <h5 class="theme"> THEME : <?php echo $enregistrements[0]['nom'] ?></h5>
+        </div>
+        <div class="col">
+            <h5 class="age"> AGE : <?php echo $enregistrements[0]['tranche_age'] ?></h5>
+        </div>
+        <div class="col">
+            <h5 class="duree"> DUREE : <?php echo $enregistrements[0]['duree'] ?></h5>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col">
+          <img src="activités/<?php echo $enregistrements[0]['id'] ?>.jpg" class="rounded-pill" width="500" height="350" id="activités">
+        </div>
+        <div class="col">
+          <h5 class="materiel"> Vous avez besoin de: </h5>
+            <?php echo $enregistrements[0]['materiel']?>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col">
+          <h6 class="deroulement"> Déroulement de l'activité: </h6>
+          <?php echo $enregistrements[0]['deroulement']?>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col">
+
+          <div class="formulaire_question">
+          <h6 id="gras">Les résultats de vos bouts de chou:</h6>
+          <form action="ajouter-photo.php" enctype="multipart/form-data" method="post">
+              <div class="form-floating mb-3">
+                <input type="text" class="form-control" placeholder="Votre pseudo" required="required" id="pseudo" name="pseudo">
+                <label for="pseudo">Votre pseudo</label>
+              </div>
+              <div class="form-floating mb-3">
+                <input type="hidden" name="MAX_FILE_SIZE" value="3000000" required="required" />
+                <input type="file" name="fichier" /><br />
+              </div>
+              <div class="col-auto">
+              <button type="submit" class="btn btn-primary rounded-pill">Envoyer</button>
               </div>
             </div>
-            <div class="row">
-              <div class="col">
-                <div class="formulaire_question">
-                <h6 id="gras">Les résultats de vos bouts de chou:</h6>
-                <form action="ajouter-photo.php" enctype="multipart/form-data" method="post">
-                    <div class="form-floating mb-3">
-                      <input type="text" class="form-control" placeholder="Votre pseudo" required="required" id="pseudo" name="pseudo">
-                      <label for="pseudo">Votre pseudo</label>
-                    </div>
-                    <div class="form-floating mb-3">
-                      <input type="hidden" name="MAX_FILE_SIZE" value="3000000" required="required" />
-                      <input type="file" name="fichier" /><br />
-                    </div>
-                    <div class="col-auto">
-                    <button type="submit" class="btn btn-primary rounded-pill">Envoyer</button>
-                    </div>
-                  </div>
-                </form>
-                </div>
-              </div>
-            </div>
-            <div class="row">
-              <div class="col">
-                <h6> Les commentaires des internautes:</h6>
-                <?php
-                // exécuter une requete MySQL
-                $requete = "SELECT projet_commentaire.titre, projet_commentaire.texte, projet_commentaire.date FROM projet_commentaire WHERE projet_commentaire.id_membre=projet_membre.id AND projet_commentaire.id_fiche=projet_fiche.id AND valide=?;";
-                $reponse = $pdo->prepare($requete);
-                $reponse->execute(array(1));
-                // récupérer tous les enregistrements dans un tableau
-                $enregistrements = $reponse->fetchAll();
-                // connaitre le nombre d'enregistrements
-                $nombreReponses = count($enregistrements);
-                // parcourir le tableau des enregistrements
-                for ($i=0; $i<count($enregistrements); $i++)
-                {
-                ?>
-                      <table>
-                        <tr>
-                          <th>Nom</th>
-                          <th>Commentaire</th>
-                          <th>Date</th>
-                        </tr>
-                        <tr>
-                          <th> <?php echo $enregistrements[$i]['titre'];?> </th>
-                          <th> <?php echo $enregistrements[$i]['texte'];?> </th>
-                          <th> <?php echo $enregistrements[$i]['date'];?> </th>
-                        </tr>
-                      </table>
-                  <?php
-                } ?>
-              </div>
-            </div>
-            <div class="row">
-              <div class="col">
-                <div class="formulaire_question">
-                <h6 id="gras">Voici le formulaire pour laisser votre commentaire</h6>
-                <form action="ajouter-commentaire-activité.php" method="post">
-                    <div class="form-floating mb-3">
-                      <input type="text" class="form-control" placeholder="Votre pseudo" required="required" id="pseudo" name="pseudo">
-                      <label for="pseudo">Votre pseudo</label>
-                    </div>
-                    <div class="form-floating mb-3">
-                      <input type="text" class="form-control" id="commentaire" placeholder="Votre commentaire" required="required" name="commentaire">
-                      <label for="commentaire">Votre commentaire</label>
-                    </div>
-                    <div class="col-auto">
-                    <button type="submit" class="btn btn-primary rounded-pill">Envoyer</button>
-                  </div>
-                </form>
-                </div>
-                </form>
-              </div>
-            </div>
+          </form>
           </div>
         </div>
+      </div>
+      <div class="row">
+        <div class="col">
+          <h6> Les commentaires des internautes:</h6>
+          //inclure les commentaires!
+        </div>
+      </div>
+      <div class="row">
+        <div class="col">
+          <div class="formulaire_question">
+          <h6 id="gras">Voici le formulaire pour laisser votre commentaire</h6>
+          <form action="ajouter-commentaire-activité.php" method="post">
+              <div class="form-floating mb-3">
+                <input type="text" class="form-control" placeholder="Votre pseudo" required="required" id="pseudo" name="pseudo">
+                <label for="pseudo">Votre pseudo</label>
+              </div>
+              <div class="form-floating mb-3">
+                <input type="text" class="form-control" id="commentaire" placeholder="Votre commentaire" required="required" name="commentaire">
+                <label for="commentaire">Votre commentaire</label>
+              </div>
+              <div class="col-auto">
+              <button type="submit" class="btn btn-primary rounded-pill">Envoyer</button>
+            </div>
+          </form>
+          </div>
+          </form>
+        </div>
+      </div>
+    </div>
 
-        </main>
+</main>
 
 <?php include "fin-page.inc.php"; ?>
