a09e7bc44477f054ea80355b2c4c5865d6a2d1c4
cperain <82218638+cperain@users.noreply.github.com>
2021-05-11 21:46:21 +0200

    Update inscription-formulaire.php

--- inscription-formulaire.php
+++ inscription-formulaire.php
@@ -10,7 +10,7 @@ require_once("connexion_base.php");
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Bootstrap CSS -->
-    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="css/main.css" rel="stylesheet">
     <link rel="shortcut icon" href="images/favicon.ico" type="images/x-icon" />
     <title>Inscription</title>
