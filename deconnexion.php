3a3fe890aab5a6008b4b6dd49ecfbb7b652542f8
cperain <82218638+cperain@users.noreply.github.com>
2021-05-11 11:14:13 +0200

    Update deconnexion.php

--- deconnexion.php
+++ deconnexion.php
@@ -5,12 +5,20 @@ require_once("connexion_base.php");
 <!DOCTYPE html>
 <html lang="fr">
   <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
+    <link href="css/main.css" rel="stylesheet">
+    <title>Déconnexion</title>
 
   </head>
   <body >
     <?php
-    unset($_SESSION['id_projet_membre']);
-    header("location:connexion.php");
+      unset($_SESSION['id_projet_membre']);
+      header("location:connexion.php");
     ?>
   </body>
 </html>
