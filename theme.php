1686578f6930c2e5dedaf0e9cd1fab10e0d047a5
cperain <82218638+cperain@users.noreply.github.com>
2021-05-12 08:14:08 +0200

    Update theme.php

--- theme.php
+++ theme.php
@@ -34,44 +34,43 @@ include "debut-page.inc.php";
     for ($i=0; $i<count($enregistrements); $i++)
     {
     ?>
-    <div class="card">
-      <div class="card-body">
-        <h5 class="card-title"><?php echo $enregistrements[$i]['titre'];?></h5>
-        <p class="card-text">
-          AGE : <?php echo $enregistrements[$i]['tranche_age'] ?> <br>
-          DUREE : <?php echo transforme($enregistrements[$i]['duree']); ?><br>
-          THEME : <?php echo $enregistrements[$i]['nom'] ?><br>
-        </p>
-      </div>
-      <div>
-        <a href="detail-fiche.php?id=<?php echo $enregistrements[$i]['id'] ?>" class="btn btn-primary rounded-pill espace">Consulter</a>
-      </div>
+      <div class="card">
+        <div class="card-body">
+          <h5 class="card-title"><?php echo $enregistrements[$i]['titre'];?></h5>
+          <p class="card-text">
+            AGE : <?php echo $enregistrements[$i]['tranche_age'] ?> <br>
+            DUREE : <?php echo transforme($enregistrements[$i]['duree']); ?><br>
+            THEME : <?php echo $enregistrements[$i]['nom'] ?><br>
+          </p>
+        </div>
+        <div>
+          <a href="detail-fiche.php?id=<?php echo $enregistrements[$i]['id'] ?>" class="btn btn-primary rounded-pill espace">Consulter</a>
+        </div>
       </div>
       <?php
-     } ?>
-    </div>
+    } ?>
+  </div>
 
-    <?php
+  <?php
 
-     $requete = "SELECT projet_fiche.*,tranche_age,nom FROM projet_fiche,projet_age,projet_theme WHERE projet_fiche.id_age = projet_age.id AND projet_fiche.id_theme = projet_theme.id AND id_theme=? AND validation =? AND pseudo !='';";
-     $reponse = $pdo->prepare($requete);
-     $reponse->execute(array($n,1));
-     // récupérer tous les enregistrements dans un tableau
-     $enregistrements = $reponse->fetchAll();
-     // connaitre le nombre d'enregistrements
-     $nombreReponses = count($enregistrements);
-     // parcourir le tableau des enregistrement
+  $requete = "SELECT projet_fiche.*,tranche_age,nom FROM projet_fiche,projet_age,projet_theme WHERE projet_fiche.id_age = projet_age.id AND projet_fiche.id_theme = projet_theme.id AND id_theme=? AND validation =? AND pseudo !='';";
+  $reponse = $pdo->prepare($requete);
+  $reponse->execute(array($n,1));
+  // récupérer tous les enregistrements dans un tableau
+  $enregistrements = $reponse->fetchAll();
+  // connaitre le nombre d'enregistrements
+  $nombreReponses = count($enregistrements);
+  // parcourir le tableau des enregistrement
 
-    if ($nombreReponses > 0)
-    {
-      echo "<h3>Vos idées</h3>";?>
-
-      <div class="cards">
-        <!-- boucle -->
-        <?php
-        for ($i=0; $i<count($enregistrements); $i++)
-        {
-        ?>
+  if ($nombreReponses > 0)
+  {
+    echo "<h3>Vos idées</h3>";?>
+    <div class="cards">
+      <!-- boucle -->
+      <?php
+      for ($i=0; $i<count($enregistrements); $i++)
+      {
+      ?>
         <div class="card">
           <div class="card-body">
             <h5 class="card-title"><?php echo $enregistrements[$i]['titre']." (@". $enregistrements[$i]['pseudo'].")";?></h5>
@@ -84,12 +83,12 @@ include "debut-page.inc.php";
           <div>
             <a href="detail-fiche?id=<?php echo $enregistrements[$i]['id'] ?>" class="btn btn-primary rounded-pill color">Consulter</a>
           </div>
-          </div>
+        </div>
           <?php
          } ?>
-        </div>
-     <?php
-      } ?>
+    </div>
+    <?php
+  } ?>
 
 </main>
 
