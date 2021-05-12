30d5af15f47950e948ae3122b1e4534f719adfba
chlmorel <82218408+chlmorel@users.noreply.github.com>
2021-05-12 10:48:45 +0200

    Update favoris.php

--- favoris.php
+++ favoris.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 require_once("connexion_base.php");
-
+$donnees['titre_page'] = "Favoris";
 
 include "debut-page.inc.php";
 ?>
@@ -56,7 +56,10 @@ include "debut-page.inc.php";
                 </div>
                 <div>
                   <a href="detail-fiche.php?id=<?php echo $favoris[$i]['id'] ?>" class="btn btn-primary rounded-pill espace">Consulter</a>
-                  <a href="supprimer-favoris.php?id=<?php echo $favoris[$i]['id'] ?>" class="btn btn-primary rounded-pill espace">Supprimer</a>
+                  <a href="supprimer-favoris.php?id=<?php echo $favoris[$i]['id'] ?>" class="btn btn-danger rounded-pill espace"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+  <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+  <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+</svg></a>
                 </div>
                 </div>
             <?php
@@ -74,9 +77,14 @@ include "debut-page.inc.php";
                    </div>
                    <div>
                      <a href="detail-fiche.php?id=<?php echo $favoris[$i]['id'] ?>" class="btn btn-primary rounded-pill color">Consulter</a>
-                     <a href="supprimer-favoris.php?id=<?php echo $favoris[$i]['id'] ?>" class="btn btn-primary rounded-pill espace">Supprimer</a>
+                     <a href="supprimer-favoris.php?id=<?php echo $favoris[$i]['id'] ?>" class="btn btn-danger rounded-pill espace">
+                       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                         <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+                         <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+                       </svg>
+                     </a>
                    </div>
-                   </div>
+                 </div>
                    <?php
                   }
 
